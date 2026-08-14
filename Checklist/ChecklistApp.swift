8d4961947f2808d3b2ddec3d16c1334fbf0119f8
aleks5230 <aleksjankowiak5230@gmail.com>
2026-08-14 23:27:23 +0200

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Aleks Jankowiak on 14/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
