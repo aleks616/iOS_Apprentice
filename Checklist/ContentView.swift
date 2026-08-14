f021e53ac211d943f6182bfdadd9cc17d403d4df
aleks5230 <aleksjankowiak5230@gmail.com>
2026-08-14 23:46:35 +0200

    Basic hardcoded checklist

--- Checklist/ContentView.swift
+++ Checklist/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            List {
+                Section(header:Text("High priority")){
+                    Text("Walk the dog")
+                    Text("Brush my teeth")
+                    Text("Learn iOS development")
+                    Text("Make dinner")
+                    Text("Do laundry")
+                    Text("Pay bills")
+                    Text("Finish homework")
+                    Text("Change internet provider")
+                    Text("Read Raywenderlich.com")
+                    Text("Clean the kitchen")
+                    Text("Wash the car")
+                }
+                Section(header:Text("Low priority")){
+                    Text("Soccer practice")
+                    Text("Eat ice cream")
+                    Text("Take vocal lessons")
+                    Text("Record hit single")
+                    Text("Learn every martial art")
+                    Text("Design costume")
+                    Text("Design crime-fighting vehicle")
+                    Text("Come up with superhero name")
+                    Text("Befriend space racoon")
+                    Text("Save the world")
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Checklist")
         }
-        .padding()
     }
 }
 
