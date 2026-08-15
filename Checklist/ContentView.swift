96987fa171ab7ad97182276e632eef8c62bbdba0
aleks5230 <aleksjankowiak5230@gmail.com>
2026-08-15 16:12:27 +0200

    chapter 10

--- Checklist/ContentView.swift
+++ Checklist/ContentView.swift
@@ -7,28 +7,39 @@
 
 import SwiftUI
 
+struct ChecklistItem: Identifiable{
+    let id = UUID()
+    var name: String
+    var isChecked: Bool = false
+}
+
 struct ContentView: View {
     @State var checklistItems=[
-        "Walk the dog",
-        "Brush my teeth",
-        "Learn iOS development",
-        "Soccer practice",
-        "Eat ice cream",
-        "Take vocal lessons",
-        "Record hit single",
-        "Learn every martial art",
-        "Design costume",
-        "Save the world",
-        "Come up with superhero name",
-        "Befriend space raccoon",
-        "Star in blockbuster movie",
+        ChecklistItem(name: "Walk the dog"),
+        ChecklistItem(name: "Brush my teeth"),
+        ChecklistItem(name:"Learn iOS development", isChecked: true),
+        ChecklistItem(name: "Soccer practice"),
+        ChecklistItem(name: "Eat ice cream", isChecked: false),
+    
     ]
     
     var body: some View {
         NavigationView{
             List {
-                ForEach(checklistItems, id: \.self){item in
-                    Text(item)
+                ForEach(checklistItems){item in
+                    HStack{
+                        Text(item.name)
+                        Spacer()
+                        Text(item.isChecked ? "✅":"◼️")
+                    }
+                    .background(Color.white)
+                    .onTapGesture {
+                        if let matchingIndex = self.checklistItems.firstIndex(where: {
+                            $0.id == item.id}){
+                            self.checklistItems[matchingIndex].isChecked.toggle()
+                        }
+                        self.printChecklistContent()
+                    }
                 }
                 .onDelete(perform: deleteListItem)
                 .onMove(perform: moveListItem)
